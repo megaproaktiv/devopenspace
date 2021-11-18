55d77cd5a0bbb0a01daf89ac0ba5fd8ede0d350c
Gernot Glawe <gglawe@tecracer.de>
2021-11-18 14:54:53 +0100

    Container Healthcheck

--- vpc/lib/vpc-stack.ts
+++ vpc/lib/vpc-stack.ts
@@ -14,6 +14,7 @@ export class VpcStack extends Stack {
 
    new aws_ssm.StringParameter(this, "basevpc", {
      stringValue: vpc.vpcId,
+     parameterName: "devopenspacevpc",
      description: "Vpc ID"
    })
 
